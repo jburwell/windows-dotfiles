ff8b2d8a9eeecd79292ba80e1972b5277eb42f40
John Burwell <jburwell@deloitte.com>
2018-10-26 16:08:27 -0400

    Adds Powershell configuration

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module PSReadLine
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+If (Test-Path Alias:curl) {Remove-Item Alias:curl} 
